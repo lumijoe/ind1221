2a103452a7eaec8abf5d8b9bc31beed1a04eb75d
lumijoe <spacedesign63@gmail.com>
2024-12-22 02:18:27 +0900

    test

--- assets/js/loadCommonParts.js
+++ assets/js/loadCommonParts.js
@@ -1,7 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // ヘッダーを読み込む
     fetch('/assets/includes/header.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('commonHeader').innerHTML = data;
+        });
+
+    // フッターを読み込む
+    fetch('/assets/includes/footer.html') // footer.htmlのパスを指定
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('commonFooter').innerHTML = data;
         });
 });
