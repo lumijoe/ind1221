8af73cbbde5c323f050ac7a794056d511197eaf5
lumijoe <spacedesign63@gmail.com>
2025-01-05 22:07:52 +0900

    tes2

--- assets/js/top.js
+++ assets/js/top.js
@@ -70,3 +70,25 @@ document.addEventListener('DOMContentLoaded', function() {
 
 }); 
 
+$(function() {
+    // アラートを利用するとjsが反映されているかがすぐわかる
+    // alert();
+    // スクロールしたとき
+    $(window).scroll(function() {
+        // スクロール量を取得
+        const scroll = $(window).scrollTop();
+        // 画面の高さを取得
+        const windowHeight = $(window).height();
+        // 各.boxに対して処理を行う
+        $(".box, .text, .text80").each(function() {
+            // それぞれの.boxの上端の座標を取得
+            const boxTop = $(this).offset().top;
+            // 条件を満たす場合はis-activeクラスを付与し、そうでない場合は削除する
+            if (scroll + windowHeight > boxTop + 300) {
+                $(this).addClass("is-active");
+            } else {
+                $(this).removeClass("is-active");
+            }
+        });
+    });
+});
