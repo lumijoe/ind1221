07148723a37a1a9920371e0ff57172d2bbbccd42
lumijoe <spacedesign63@gmail.com>
2025-01-26 22:43:02 +0900

    test

--- component-formcontact.php
+++ component-formcontact.php
@@ -1,4 +1,4 @@
-<!-- includes/formcontact.html -->
+<!-- component-formcontact.php -->
 <section class="contact-ir" id="ifcontact">
     <h2 style="margin-top:0px; margin-bottom:8px;">CONTACT US</h2>
     <p style="margin-top:0px; margin-bottom:16px;">まずはお気軽にご相談ください</p>
