9cabf1faa3d64787746998f9267eae11a8250e14
小城 <kojolumi@gamil.com>
2025-01-30 13:04:19 +0900

    sitemap

---
+++ page-sitemap.php
@@ -0,0 +1,17 @@
+<!-- page-sitemap.php -->
+<?php
+
+/**
+ * Template Name: page-sitemap
+ * Description: This is the template
+ */
+
+get_header();
+?>
+<!-- サイトマップページ -->
+<main>
+
+</main>
+<?php
+    get_footer(); 
+?>
