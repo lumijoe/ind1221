a85c12dba87a8a1e158ff798f6050c2887d75242
joe <spacedesign63@gmail.com>
2025-06-08 13:26:52 +0900

    footer more news link

--- footer-products.php
+++ footer-products.php
@@ -4,11 +4,13 @@
 //====================================================
 ?>
 </main>
- <!-- フッター -->
- <footer class="footer">
+<!-- フッター -->
+<footer class="footer">
     <div class="footer-inner text80">
         <div class="footer-section">
-            <a href="<?php echo esc_url(home_url()); ?>/company/"><h3 class="footer-nav-title">企業情報</h3></a>
+            <a href="<?php echo esc_url(home_url()); ?>/company/">
+                <h3 class="footer-nav-title">企業情報</h3>
+            </a>
             <ul>
                 <li><a href="<?php echo esc_url(home_url()); ?>/company/#message">メッセージ</a></li>
                 <li><a href="<?php echo esc_url(home_url()); ?>/company/#about">会社概要</a></li>
@@ -16,7 +18,9 @@
             </ul>
         </div>
         <div class="footer-section">
-            <a href="<?php echo esc_url(home_url()); ?>/products/"><h3 class="footer-nav-title">製品情報</h3></a>
+            <a href="<?php echo esc_url(home_url()); ?>/products/">
+                <h3 class="footer-nav-title">製品情報</h3>
+            </a>
             <ul>
                 <li><a href="<?php echo esc_url(home_url()); ?>/products/category-a/">検査装置・設備</a></li>
                 <li><a href="<?php echo esc_url(home_url()); ?>/products/category-b/">工具・周辺機器</a></li>
@@ -24,7 +28,9 @@
             </ul>
         </div>
         <div class="footer-section">
-            <a href="<?php echo esc_url(home_url()); ?>/ir/"><h3 class="footer-nav-title">IR情報</h3></a>
+            <a href="<?php echo esc_url(home_url()); ?>/ir/">
+                <h3 class="footer-nav-title">IR情報</h3>
+            </a>
             <ul>
                 <li><a href="<?php echo esc_url(home_url()); ?>/ir/#irnews">IRニュース</a></li>
                 <li><a href="<?php echo esc_url(home_url()); ?>/ir/#irlibrary">IRライブラリー</a></li>
@@ -34,7 +40,9 @@
             </ul>
         </div>
         <div class="footer-section">
-            <a href="<?php echo esc_url(home_url()); ?>/sustainability/"><h3 class="footer-nav-title">サステナビリティ</h3></a>
+            <a href="<?php echo esc_url(home_url()); ?>/sustainability/">
+                <h3 class="footer-nav-title">サステナビリティ</h3>
+            </a>
             <ul>
                 <li><a href="<?php echo esc_url(home_url()); ?>/sustainability/#sustainabilitynews">サステナニュース</a></li>
                 <li><a href="<?php echo esc_url(home_url()); ?>/sustainability/#initiatives">取り組み</a></li>
@@ -44,11 +52,19 @@
             </ul>
         </div>
         <div class="footer-section">
-            <a href="<?php echo esc_url(home_url()); ?>/news/" ><h3 class="h3borderbottomnone footer-nav-title">お知らせ</h3></a>
+            <a href="<?php echo esc_url(home_url()); ?>/archives/news/">
+                <h3 class="h3borderbottomnone footer-nav-title">お知らせ</h3>
+            </a>
             <!-- <a href="/recruit/" ><h3 class="h3borderbottomnone footer-nav-title">採用情報</h3></a> -->
-            <a href="https://ikkosha.co.jp/staff/" target="_blank" rel="noopener noreferrer" style="display:flex; align-items: center;"><h3 class="h3borderbottomnone footer-nav-title">採用情報</h3><img src="https://lkcodetest.sakura.ne.jp/ind1221/wp-content/uploads/images/icon_newopen.svg" alt="" width="25" height="25" style="padding-bottom:10px;"></a>
-            <a href="<?php echo esc_url(home_url()); ?>/ir/#iffaq" ><h3 class="h3borderbottomnone footer-nav-title">よくあるご質問</h3></a>
-            <a href="<?php echo esc_url(home_url()); ?>/contact/" target="_blank" rel="noopener noreferrer"><h3 class="h3borderbottomnone footer-nav-title">お問い合わせ</h3></a>
+            <a href="https://ikkosha.co.jp/staff/" target="_blank" rel="noopener noreferrer" style="display:flex; align-items: center;">
+                <h3 class="h3borderbottomnone footer-nav-title">採用情報</h3><img src="https://lkcodetest.sakura.ne.jp/ind1221/wp-content/uploads/images/icon_newopen.svg" alt="" width="25" height="25" style="padding-bottom:10px;">
+            </a>
+            <a href="<?php echo esc_url(home_url()); ?>/ir/#iffaq">
+                <h3 class="h3borderbottomnone footer-nav-title">よくあるご質問</h3>
+            </a>
+            <a href="<?php echo esc_url(home_url()); ?>/contact/" target="_blank" rel="noopener noreferrer">
+                <h3 class="h3borderbottomnone footer-nav-title">お問い合わせ</h3>
+            </a>
         </div>
     </div>
     <div class="footer-level-two">
@@ -57,7 +73,7 @@
                 <p class="footer-logo-caption">エネルギーで産業を支える。</p>
                 <div class="footer-logo">
                     <img src="https://lkcodetest.sakura.ne.jp/ind1221/wp-content/uploads/images/logo.png" alt="会社ロゴ" loading="lazy">
-                    <h2>yCOMPANY</h2> 
+                    <h2>yCOMPANY</h2>
                 </div>
             </div>
             <copy class="copyright">&copy;yCompany All rights reserved.</copy>
@@ -68,20 +84,20 @@
             <li><a href="https://ikkosha.co.jp/privacy-policy/" target="_blank" rel="noopener noreferrer" style="display:flex; align-items: center;">プライバシーポリシー<img src="https://lkcodetest.sakura.ne.jp/ind1221/wp-content/uploads/images/icon_newopen.svg" alt="" width="15" height="15" style="padding-left:2px;"></a></li>
         </ul>
     </div>
-   
+
 
 </footer>
 
-<section  id="footer-nav" class="footer-nav hidden">
+<section id="footer-nav" class="footer-nav hidden">
     <ul class="footer-nav-wrapper">
         <li class="footer-nav-item"><a href="<?php echo esc_url(home_url()); ?>/products/category-a/index.html" style="display: flex; flex-direction:column; align-items: center;"><img src="https://lkcodetest.sakura.ne.jp/ind1221/wp-content/uploads/images/icon_01.svg" alt="" width="30" height="30" style="background-color: #e6ebf0;"><span style="font-size:11px; padding-top:3px;">検査装置・設備</span></a></li>
         <li class="footer-nav-item"><a href="<?php echo esc_url(home_url()); ?>/products/category-b/index.html" style="display: flex; flex-direction:column; align-items: center;"><img src="https://lkcodetest.sakura.ne.jp/ind1221/wp-content/uploads/images/icon_02.svg" alt="" width="30" height="30" style="background-color: #e6ebf0;"><span style="font-size:11px; padding-top:3px;">工具・周辺機器</span></a></li>
         <li class="footer-nav-item" style="border-right:1px solid #eee;"><a href="<?php echo esc_url(home_url()); ?>/products/category-c/index.html" style="display: flex; flex-direction:column; align-items: center;"><img src="https://lkcodetest.sakura.ne.jp/ind1221/wp-content/uploads/images/icon_031.svg" alt="" width="30" height="30" style="background-color: #e6ebf0;"><span style="font-size:11px; padding-top:3px;">工業炉</span></a></li>
-        
+
     </ul>
     <ul class="footer-nav-wrapper-cta">
-         <!-- ボタン -->
-         <li class="menu-item menu-item-button">
+        <!-- ボタン -->
+        <li class="menu-item menu-item-button">
             <a href="#" class="button item-level0ne" style="color: #fff !important;"><img src="https://lkcodetest.sakura.ne.jp/ind1221/wp-content/uploads/images/icon_book_white.svg" alt="" width="30" height="30">カタログ一覧</a>
         </li>
         <li class="menu-item menu-item-button" style="padding-right:20px;">
@@ -95,11 +111,12 @@
 <?php wp_footer(); ?>
 
 <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
-    <script src="<?php echo get_template_directory_uri(); ?>/assets/js/loadCommonParts.js"></script>
-    <script src="<?php echo get_template_directory_uri(); ?>/assets/js/megamenu.js"></script>
-    <script src="<?php echo get_template_directory_uri(); ?>/assets/js/top.js"></script>
-    <script src="<?php echo get_template_directory_uri(); ?>/assets/js/category.js"></script>
-    <script src="<?php echo get_template_directory_uri(); ?>/assets/js/products-search.js"></script>
-    <script src="<?php echo get_template_directory_uri(); ?>/assets/js/common.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/loadCommonParts.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/megamenu.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/top.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/category.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/products-search.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/common.js"></script>
 </body>
+
 </html>
