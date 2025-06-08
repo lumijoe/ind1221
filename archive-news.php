70c0bc6bdbec8480115186166ca09d1721827d16
joe <spacedesign63@gmail.com>
2025-06-08 14:40:10 +0900

    add componentnavnews and page txt

--- archive-news.php
+++ archive-news.php
@@ -8,243 +8,116 @@
 
 get_header('news');
 ?>
+<style>
+    .tab-pane {
+        display: none;
+    }
+
+    .tab-pane.active.show {
+        display: block;
+    }
+
+    .nav-link.active {
+        font-weight: bold;
+        border-bottom: 2px solid black;
+    }
+</style>
 <main>
-    <!-- カテゴリタブ -->
-    <div class="l-category-tab">
-        <button class="is-current"><a href="<?php echo home_url(''); ?>">すべて</a></button>
-        <button><a href="<?php echo home_url(''); ?>">製品情報</a></button>
-        <button><a href="<?php echo home_url(''); ?>">IR</a></button>
-        <button><a href="<?php echo home_url(''); ?>">サステナビリティ</a></button>
-        <button><a href="<?php echo home_url(''); ?>">お知らせ</a></button>
-    </div>
+    <!-- カテゴリタブ component-tagnavnews.php -->
+    <?php get_template_part('component', 'tagnavnews'); ?>
 
-    <!-- サイドバー -->
-    <div class="l-side-grid">
-        <!-- 記事セクション -->
-        <section class="l-article">
-            <?php if (have_posts()) : ?>
-                <ul>
-                    <hr class="article-top-hr">
-                    <?php while (have_posts()) : the_post(); ?>
-                        <li class="sp-article-wrapper">
-                            <div class="sp-article">
-                                <!-- ACF投稿日時 -->
-                                <date class="post-date"><?php echo get_the_date('Y.m.d'); ?></date>
+    <!-- お知らせ一覧 -->
+    <section class="news-archive-section">
+        <div class="container">
+            <div class="news-toppage-ttl">
+                <h2 style="width: 100%;">お知らせ一覧</h2>
+            </div>
+            <p class="news-toppage-ttl-en">NEWS</p>
+            <?php
+            $args = array(
+                'post_type'      => array('news', 'irnews'), // 両方を指定
+                'posts_per_page' => 10,
+                'post_status'    => 'publish',
+                'orderby'        => 'date',
+                'order'          => 'DESC',
+                'paged'          => get_query_var('paged') ? get_query_var('paged') : 1,
+            );
 
-                                <!-- カテゴリ表示 -->
+            $mixed_query = new WP_Query($args);
+            ?>
+
+            <?php if ($mixed_query->have_posts()) : ?>
+                <div class="news-list">
+                    <?php while ($mixed_query->have_posts()) : $mixed_query->the_post(); ?>
+                        <div class="news-list-wrapper">
+                            <div class="news-img-area">
                                 <?php
-                                $terms = get_the_terms(get_the_ID(), 'newscategory');
-                                if ($terms && !is_wp_error($terms)) :
-                                    $first_term = $terms[0];
-                                    $category_output = $first_term->name;
-                                else :
-                                    $category_output = 'カテゴリなし';
-                                endif;
+                                $post_type = get_post_type(); // 投稿タイプ判定
+                                $image = ($post_type === 'irnews') ? get_field('irpost_imageurl') : get_field('post_imageurl');
+                                $title = ($post_type === 'irnews') ? get_field('irpost_title') : get_field('post_title');
+                                $terms = get_the_terms(get_the_ID(), ($post_type === 'irnews') ? 'irnewscategory' : 'newscategory');
+                                $category = ($terms && !is_wp_error($terms)) ? $terms[0]->name : 'カテゴリなし';
                                 ?>
-                                <!-- span新着タグ -->
+
+                                <?php if ($image) : ?>
+                                    <img src="<?php echo esc_url($image); ?>" class="news-cap" alt="">
+                                <?php endif; ?>
+
                                 <?php
-                                // 投稿日と現在の日付を取得
                                 $post_date = get_the_date('Y-m-d');
                                 $post_datetime = new DateTime($post_date);
                                 $now = new DateTime();
-
-                                // 投稿が過去の日付かチェック（未来記事を除外）
-                                if ($post_datetime <= $now) {
-                                    $interval = $now->diff($post_datetime)->days;
-
-                                    // 7日以内ならNEWを表示
-                                    if ($interval < 7) :
+                                $interval = $now->diff($post_datetime)->days;
+                                if ($interval < 7) :
                                 ?>
-                                        <span class="tag_new">NEW</span>
-                                <?php
-                                    endif;
-                                }
-                                ?>
-                                <!-- カテゴリタグ -->
-                                <p class="item-category"><?php echo esc_html($category_output); ?></p>
+                                    <span class="mark-newsarrival text80">NEW</span>
+                                <?php endif; ?>
                             </div>
 
-                            <!-- ACFタイトル -->
-                            <?php if (get_field('post_title')) : ?>
-                                <a href="<?php the_permalink(); ?>"><?php the_field('post_title'); ?></a>
-                            <?php endif; ?>
-
-                        </li>
-                        <hr>
-                    <?php endwhile; ?>
-
-                </ul>
-
-                <?php the_posts_pagination(); ?>
-
-            <?php else : ?>
-                <p>お知らせはありません。</p>
-            <?php endif; ?>
-        </section>
-
-        <section class="news-archive-section">
-            <div class="container">
-                <div class="news-toppage-ttl">
-                    <h2 style="width: 100%;">お知らせ一覧</h2>
-                </div>
-                <p class="news-toppage-ttl-en">NEWS</p>
-                <?php
-                $args = array(
-                    'post_type'      => array('news', 'irnews'), // 両方を指定
-                    'posts_per_page' => 10,
-                    'post_status'    => 'publish',
-                    'orderby'        => 'date',
-                    'order'          => 'DESC',
-                    'paged'          => get_query_var('paged') ? get_query_var('paged') : 1,
-                );
-
-                $mixed_query = new WP_Query($args);
-                ?>
-
-                <?php if ($mixed_query->have_posts()) : ?>
-                    <div class="news-list">
-                        <?php while ($mixed_query->have_posts()) : $mixed_query->the_post(); ?>
-                            <div class="news-list-wrapper">
-                                <div class="news-img-area">
+                            <article class="news-item" style="border-bottom:none!important; align-items:flex-start;">
+                                <div class="news-item-level1">
+                                    <time><?php echo get_the_date('Y.m.d'); ?></time>
                                     <?php
-                                    $post_type = get_post_type(); // 投稿タイプ判定
-                                    $image = ($post_type === 'irnews') ? get_field('irpost_imageurl') : get_field('post_imageurl');
-                                    $title = ($post_type === 'irnews') ? get_field('irpost_title') : get_field('post_title');
-                                    $terms = get_the_terms(get_the_ID(), ($post_type === 'irnews') ? 'irnewscategory' : 'newscategory');
-                                    $category = ($terms && !is_wp_error($terms)) ? $terms[0]->name : 'カテゴリなし';
+                                    if ($terms && !is_wp_error($terms)) :
+                                        $first_term = $terms[0];
+                                        $category_output = $first_term->name;
+                                    else :
+                                        $category_output = 'カテゴリなし';
+                                    endif;
                                     ?>
-
-                                    <?php if ($image) : ?>
-                                        <img src="<?php echo esc_url($image); ?>" class="news-cap" alt="">
-                                    <?php endif; ?>
-
-                                    <?php
-                                    $post_date = get_the_date('Y-m-d');
-                                    $post_datetime = new DateTime($post_date);
-                                    $now = new DateTime();
-                                    $interval = $now->diff($post_datetime)->days;
-                                    if ($interval < 7) :
-                                    ?>
-                                        <span class="mark-newsarrival text80">NEW</span>
-                                    <?php endif; ?>
+                                    <span class="news-category"><?php echo esc_html($category_output); ?></span>
                                 </div>
 
-                                <article class="news-item" style="border-bottom:none!important; align-items:flex-start;">
-                                    <div class="news-item-level1">
-                                        <time><?php echo get_the_date('Y.m.d'); ?></time>
-                                        <?php
-                                        if ($terms && !is_wp_error($terms)) :
-                                            $first_term = $terms[0];
-                                            $category_output = $first_term->name;
-                                        else :
-                                            $category_output = 'カテゴリなし';
-                                        endif;
-                                        ?>
-                                        <span class="news-category"><?php echo esc_html($category_output); ?></span>
-                                    </div>
-
-                                    <a href="<?php the_permalink(); ?>" class="news-item-txt" style="overflow:hidden; display:-webkit-box; -webkit-box-orient: vertical; -webkit-line-clamp: 3;">
-                                        <?php echo esc_html($title); ?>
-                                        <?php if ($post_type === 'irnews') : ?>
-                                            <img src="https://lkcodetest.sakura.ne.jp/ind1221/wp-content/uploads/images/icon_pdf.svg" alt="" style="width:23px">
-                                        <?php endif; ?>
-                                    </a>
-                                </article>
-                            </div>
-                        <?php endwhile; ?>
-                    </div>
-
-                    <!-- ページネーション -->
-                    <div class="pagination">
-                        <?php
-                        echo paginate_links(array(
-                            'total' => $mixed_query->max_num_pages,
-                        ));
-                        ?>
-                    </div>
-
-                    <?php wp_reset_postdata(); ?>
-
-                <?php else : ?>
-                    <p>お知らせはまだありません。</p>
-                <?php endif; ?>
-            </div>
-        </section>
-
-
-        <!-- サステナビリティニュースセクション -->
-        <section class="news-section" style="padding-top:40px;" id="sustainabilitynews">
-            <div class="container news-container">
-                <div class="news-grid">
-
-                    <div class="news-main" style="width:70%!important; padding-left:0px; padding-right:0px;">
-                        <div class="news-toppage-ttl">
-                            <h2 style="width:auto">サステナビリティニュース</h2>
-
+                                <a href="<?php the_permalink(); ?>" class="news-item-txt" style="overflow:hidden; display:-webkit-box; -webkit-box-orient: vertical; -webkit-line-clamp: 3;">
+                                    <?php echo esc_html($title); ?>
+                                    <?php if ($post_type === 'irnews') : ?>
+                                        <img src="https://lkcodetest.sakura.ne.jp/ind1221/wp-content/uploads/images/icon_pdf.svg" alt="" style="width:23px">
+                                    <?php endif; ?>
+                                </a>
+                            </article>
                         </div>
-                        <p class="news-toppage-ttl-en">SUSTAINABILITY NEWS</p>
-                        <div class="news-list">
-                            <div class="news-list-wrapper">
-                                <img src="https://lkcodetest.sakura.ne.jp/ind1221/wp-content/uploads/images/img_news_sus_01.png" alt="" class="news-cap">
-                                <article class="news-item product" style="align-items:flex-start;">
-                                    <div class="news-item-level1">
-                                        <time>2024.03.20</time>
-                                        <span class="news-category">サステナビリティ</span>
-                                    </div>
-                                    <a href="#" class="news-item-txt">地域の清掃活動支援について</a>
-                                </article>
-                            </div>
-
-                            <div class="news-list-wrapper">
-                                <img src="https://images.unsplash.com/photo-1451847251646-8a6c0dd1510c?auto=format&fit=crop&q=80" alt="サステナビリティ" class="news-cap">
-                                <article class="news-item product" style="align-items:flex-start;">
-                                    <div class="news-item-level1">
-                                        <time>2024.03.10</time>
-                                        <span class="news-category">サステナビリティ</span>
-                                    </div>
-                                    <a href="#" class="news-item-txt">環境への取り組みについて</a>
-                                </article>
-                            </div>
-
-                            <div class="news-list-wrapper">
-                                <img src="https://images.unsplash.com/photo-1504711434969-e33886168f5c?auto=format&fit=crop&q=80" alt="" class="news-cap">
-                                <article class="news-item product" style="align-items:flex-start;">
-                                    <div class="news-item-level1">
-                                        <time>2024.03.19</time>
-                                        <span class="news-category">サステナビリティ</span>
-                                    </div>
-                                    <a href="#" class="news-item-txt">当社の取り組みが新聞に掲載されました</a>
-                                </article>
-                            </div>
-                        </div>
-                    </div>
+                    <?php endwhile; ?>
                 </div>
-            </div>
-        </section>
 
-        <!-- ESGセクション -->
-        <section class="esg-section" id="initiatives">
-            <div class="container" style="max-width:1000px!important;">
-                <h2>ESG（環境・社会・ガバナンス）への取り組み</h2>
-                <div class="sustainability-grid" style="grid-template-columns: 1fr 1fr 1fr!important;">
-                    <div class="sustainability-content" id="environment">
-                        <img src="https://lkcodetest.sakura.ne.jp/ind1221/wp-content/uploads/images/img_sus_e.png" alt="環境" loading="lazy" width="213" height="319">
-                        <h3>環境</h3>
-                        <p>環境保護に向けた取り組みとして、エネルギー効率の向上や廃棄物の削減に努めています。</p>
-                    </div>
-                    <div class="sustainability-content" id="society">
-                        <img src="https://lkcodetest.sakura.ne.jp/ind1221/wp-content/uploads/images/img_sus_s.png" alt="社会" loading="lazy">
-                        <h3>社会</h3>
-                        <p>地域社会への貢献や多様性の尊重を通じて、持続可能な社会の実現を目指しています。</p>
-                    </div>
-                    <div class="sustainability-content" id="governance">
-                        <img src="https://lkcodetest.sakura.ne.jp/ind1221/wp-content/uploads/images/img_sus_g.png" alt="ガバナンス" loading="lazy">
-                        <h3>ガバナンス</h3>
-                        <p>透明性のある経営と倫理的なビジネス慣行を通じて、信頼される企業を目指します。</p>
-                    </div>
+                <!-- ページネーション -->
+                <div class="pagination">
+                    <?php
+                    echo paginate_links(array(
+                        'total' => $mixed_query->max_num_pages,
+                    ));
+                    ?>
                 </div>
-            </div>
-        </section>
+
+                <?php wp_reset_postdata(); ?>
+
+            <?php else : ?>
+                <p>お知らせはまだありません。</p>
+            <?php endif; ?>
+        </div>
+    </section>
+
+
+
 </main>
 <?php
 get_footer();
---
+++ component-tagnavnews.php
@@ -0,0 +1,148 @@
+<!-- component-tagnavnews.php -->
+<section id="newsNavtag">
+    <nav>
+        <div class="nav nav-tabs" id="newsNavtag-tab" role="tablist">
+            <button
+                class="nav-link active"
+                id="newsNavtag-all-tab"
+                data-bs-toggle="tab"
+                data-bs-target="#newsNavtag-all"
+                type="button"
+                role="tab"
+                aria-controls="newsNavtag-all"
+                aria-selected="true">
+                すべて
+            </button>
+            <button
+                class="nav-link"
+                id="newsNavtag-news-tab"
+                data-bs-toggle="tab"
+                data-bs-target="#newsNavtag-news"
+                type="button"
+                role="tab"
+                aria-controls="newsNavtag-news"
+                aria-selected="false">
+                お知らせ
+            </button>
+            <button
+                class="nav-link"
+                id="newsNavtag-product-tab"
+                data-bs-toggle="tab"
+                data-bs-target="#newsNavtag-product"
+                type="button"
+                role="tab"
+                aria-controls="newsNavtag-product"
+                aria-selected="false">
+                製品情報
+            </button>
+            <button
+                class="nav-link"
+                id="newsNavtag-sustainability-tab"
+                data-bs-toggle="tab"
+                data-bs-target="#newsNavtag-sustainability"
+                type="button"
+                role="tab"
+                aria-controls="newsNavtag-sustainability"
+                aria-selected="false"
+                disabled>
+                サステナビリティ
+            </button>
+            <button
+                class="nav-link"
+                id="newsNavtag-ir-tab"
+                data-bs-toggle="tab"
+                data-bs-target="#newsNavtag-ir"
+                type="button"
+                role="tab"
+                aria-controls="newsNavtag-ir"
+                aria-selected="false"
+                disabled>
+                IR
+            </button>
+        </div>
+    </nav>
+    <div class="tab-content" id="newsNavtag-tabContent">
+        <div
+            class="tab-pane fade show active"
+            id="newsNavtag-all"
+            role="tabpanel"
+            aria-labelledby="newsNavtag-all-tab"
+            tabindex="0">
+            全てのニュースの情報が入ります
+        </div>
+        <div
+            class="tab-pane fade"
+            id="newsNavtag-news"
+            role="tabpanel"
+            aria-labelledby="newsNavtag-profile-news"
+            tabindex="0">
+            お知らせの文章が入ります
+        </div>
+        <div
+            class="tab-pane fade"
+            id="newsNavtag-product"
+            role="tabpanel"
+            aria-labelledby="newsNavtag-product"
+            tabindex="0">
+            製品に関しての文章が入ります
+        </div>
+        <div
+            class="tab-pane fade"
+            id="newsNavtag-sustainability"
+            role="tabpanel"
+            aria-labelledby="newsNavtag-sustainability"
+            tabindex="0">
+            サステナビリティに関しての文章が入ります
+        </div>
+        <div
+            class="tab-pane fade"
+            id="newsNavtag-ir"
+            role="tabpanel"
+            aria-labelledby="newsNavtag-ir"
+            tabindex="0">
+            IRに関しての文章が入ります
+        </div>
+        <!-- <div
+                    class="tab-pane fade"
+                    id="newsNavtag-disabled"
+                    role="tabpanel"
+                    aria-labelledby="newsNavtag-disabled-tab"
+                    tabindex="0">
+                    disに関しての文章が入ります
+                </div> -->
+    </div>
+</section>
+
+
+
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        const section = document.getElementById("newsNavtag");
+        const tabButtons = section.querySelectorAll('[role="tab"]');
+        const tabContents = section.querySelectorAll('[role="tabpanel"]');
+
+        tabButtons.forEach(button => {
+            // disabled タブはスキップ
+            if (button.hasAttribute('disabled')) return;
+
+            button.addEventListener("click", function() {
+                // 1. 全ボタンから active を外す
+                tabButtons.forEach(btn => btn.classList.remove("active"));
+                // 2. 全パネルから active/show を外す
+                tabContents.forEach(panel => {
+                    panel.classList.remove("active", "show");
+                });
+
+                // 3. クリックされたボタンに active をつける
+                this.classList.add("active");
+
+                // 4. 対象のタブパネルを表示
+                const targetId = this.getAttribute("data-bs-target");
+                const targetPanel = section.querySelector(targetId);
+                if (targetPanel) {
+                    targetPanel.classList.add("active", "show");
+                }
+            });
+        });
+    });
+</script>
